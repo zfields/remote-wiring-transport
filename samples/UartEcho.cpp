de2ea31c1e7b9daa6d0e671403a360d769613a30
Zachary J. Fields <zachary_fields@yahoo.com>
2017-03-28 22:45:14 -0700

    Use C system calls in UartEcho sample

--- samples/UartEcho.cpp
+++ samples/UartEcho.cpp
@@ -1,72 +1,61 @@
-/*
-UartSerial Echo Sample
-======================
+/* Created and copyrighted by Kevin D. Sidwar. Offered as open source under the MIT License (MIT). */
 
-This sample demonstrates the serialEvent callback ability of serial wiring
-as well as basic transmit and receive. It is meant to be run with an
-Arduino attached to your machine via USB cable.
-
-The Arduino is programmed with the SerialEvent sample sketch which reads
-characters in on the serialport until a newline is encountered. Once the
-newline is received it transmits the entire string back out over serial.
-That transmission triggers the serial_event() callback in the code below.
-*/
-
-#include <serial_wiring>
-
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-#include <iostream>
+#include <serial_wiring>
 
-void onSerialEvent (void *context_) {
+void onSerialEvent (void * context_) {
     // Cast the context into a Stream object
     Stream * s = reinterpret_cast<Stream *>(context_);
 
-    while(s->available())
-    {
-        std::cout << (char)s->read() << std::flush;
+    while (s->available()) {
+        ::printf("%c", (char)s->read());
+        ::fflush(stdout);
     }
 }
 
 int main (int argc, char * argv[]) {
-    std::cout << "*****************************************************" << std::endl;
-    std::cout << "** The \"Examples > 04.Communication > SerialEvent\" **" << std::endl;
-    std::cout << "** sketch must be deployed to the Arduino in order **" << std::endl;
-    std::cout << "** for the sample to work correctly.               **" << std::endl;
-    std::cout << "*****************************************************" << std::endl;
+    ::printf("*****************************************************\n");
+    ::printf("** The \"Examples > 04.Communication > SerialEvent\" **\n");
+    ::printf("** sketch must be deployed to the Arduino in order **\n");
+    ::printf("** for the sample to work correctly.               **\n");
+    ::printf("*****************************************************\n");
 
-    if ( argc < 2 ) { std::cout << "Usage: " << argv[0] << " <serial device descriptor>" << std::endl; return -1; }
+    if ( argc < 2 ) { ::printf("Usage: %s <serial device descriptor>\n", argv[0]); return -1; }
 
     serial_wiring::UartSerial usb(argv[1]);
 
-    usb.begin(9600);  // The sample Arduino sketch is expecting a baud rate of 9600
-    usb.registerSerialEventCallback(onSerialEvent, &usb);
+    usb.begin(9600); // The sample Arduino sketch is expecting a baud rate of 9600
+    usb.registerSerialEventCallback(::onSerialEvent, &usb);
 
-    // By default Arduinos go through a hard reset when they receive a new
-    // serial connection. The following sleep() is to allow it time to
-    // boot up before sending serial data.
-    ::sleep(2);
+    // Allow the Arduino time to boot-up before sending serial data.
+    ::sleep(1);
 
-    // Get user input
+    // Prompt for user input
+    ::printf("Type a message to send to the Arduino, then press <Enter>: ");
+
+    // Capture user input
     char * user_input_buffer = NULL;
     size_t user_input_buffer_length = 0;
-    std::cout << "Type a message to send to the Arduino, then press <Enter>: ";
-    getline(&user_input_buffer, &user_input_buffer_length, stdin);
-    std::cout << std::endl;
+    ::getline(&user_input_buffer, &user_input_buffer_length, stdin);
     user_input_buffer_length = ::strnlen(user_input_buffer, user_input_buffer_length);
 
     // Send the user input to the Arduino sketch to echo back
     for (size_t i = 0 ; i < user_input_buffer_length ; ++i) {
         usb.write(user_input_buffer[i]);
     }
-    free(user_input_buffer);
-
-    std::cout << "Arduino says..." << std::endl;
+    ::free(user_input_buffer);
 
     // Give the Arduino time to send the string back over the serial line
-    sleep(1);
+    ::printf("\nArduino says...\n");
+    ::sleep(1);
 
     usb.end();
     return 0;
 }
+
+/* Created and copyrighted by Kevin D. Sidwar. Offered as open source under the MIT License (MIT). */
+
